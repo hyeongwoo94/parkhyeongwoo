2a12721204114457453536880c6754d21a9f4b31
hyeongwoo94 <87638160+hyeongwoo94@users.noreply.github.com>
2025-10-18 00:57:32 +0900

    오타수정

--- src/shared/en.ts
+++ src/shared/en.ts
@@ -1,45 +1,46 @@
 export const en = {
-    common: {
-        code_review: "Code view",
+  common: {
+    code_review: "Code view",
+  },
+  headerMenu: {
+    memo: "React Note",
+    count: "useState",
+    todo: "Todo",
+    weather: "Weather",
+    shop: "Shop",
+    ERP: "ERP",
+  },
+  home: {
+    say: {
+      text_1: "Hellow My name is",
+      text_2: "",
+      text_3: "My age is",
+      mo_text: "Please touch and hold the box for 1 second, then drag it.",
     },
-    headerMenu:{
-        memo: "React Note",
-        count: "useState",
-        todo: "Todo",
-        weather: "Weather",
-        shop: "Shop",
-        ERP: "ERP"
+    loading: {
+      text_1: "Crack the code",
+      text_2: "and you’ll get a sneak peek",
+      text_3: "at my portfolio...!",
     },
-    home: {
-        say:{
-            text_1:'Hellow My name is',
-            text_2:'',
-            text_3:'My age is',
-            mo_text:"Please touch and hold the box for 1 second, then drag it."
-        },
-        loading:{
-            text_1:'Crack the code',
-            text_2:'and you’ll get a sneak peek',
-            text_3:'at my portfolio...!',
-        },
-        quiz:{
-            BothRight:'Well, well… looking for little old me? Hit the button and find out',
-            NameRight:'You guessed my name! Click on the age hint.',
-            AgeRight:'You guessed my age! Click on the name hint.',
-            BothWrong:'Oh, look at you… sneaking in here!',
-            HintName:'My family name is Park',
-            HintAge:'I was born in 1994',
-            NameHint:"Name Hint",
-            AgeHint:"Age Hint",
-        },
-        item1:'Park Hyeong woo',
-        item2:'Brad Pitt',
-        item3:'Choi Hyeong woo',
-        item4:'Smith',
-        wellcom:'Here comes your new favorite person!'
+    quiz: {
+      BothRight:
+        "Well, well… looking for little old me? Hit the button and find out",
+      NameRight: "You guessed my name! Click on the age hint.",
+      AgeRight: "You guessed my age! Click on the name hint.",
+      BothWrong: "Oh, look at you… sneaking in here!",
+      HintName: "My family name is Park",
+      HintAge: "I was born in 1994",
+      NameHint: "Name Hint",
+      AgeHint: "Age Hint",
     },
-    login: {
+    item1: "Park Hyeong Woo",
+    item2: "Brad Pitt",
+    item3: "Choi Hyeong Woo",
+    item4: "Smith",
+    wellcom: "Here comes your new favorite person!",
+  },
+  login: {
     welcome: "다시 오신 것을 환영합니다",
     logout: "로그아웃",
-    },
+  },
 };
